7a3db9499d79f3f4f10925c8505de56e0ea11c4c
Zee <xiovacc@gmail.com>
2022-05-12 12:02:11 -0400

    fix enemy robot health/death

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,8 +8,11 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
+    // repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
+    
     // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    //window.alert("Welcome to Robot Gladiators!");
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -67,10 +70,11 @@ var fight = function(enemyName) {
 } else {
     window.alert("You need to choose a valid option. Try again!");
   }
+    }
 }
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-  }
-
-  fight();
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
+}
