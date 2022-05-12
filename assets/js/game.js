b1dc6b601ebd0ab573f8070939718414363edb60
Zee <xiovacc@gmail.com>
2022-05-12 15:49:39 -0400

    lesson 3 start

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,7 +68,24 @@ var fight = function(enemyName) {
 
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 20;
-    fight(pickedEnemyName);
+    // let player know what round they are in
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! " + (i + 1));
+
+        //pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+
+        //reset enemyHealth before starting new fight
+        enemyHealth = 50;
+
+        //use debugger to pause script from running and check what's going on at that moment in the code
+        //debugger
+        
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!")
+        break;
+    }
 }
